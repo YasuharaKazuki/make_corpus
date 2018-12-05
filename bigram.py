836d8605a29e2209da8770acb3a30d5bf2ac12c6
YasuharaKazuki <yasuhara.kazuki@g.sp.m.is.nagoya-u.ac.jp>
2018-12-05 15:31:07 +0900

    modify

--- bigram.py
+++ bigram.py
@@ -14,7 +14,7 @@ def main():
     with open(args.monogram_file, 'r') as r_file:
         monogram = [line.rstrip() for line in r_file]
 
-    with open('kasu.txt', 'w') as w_file:
+    with open('bigram.txt', 'w') as w_file:
         for monogram1 in monogram:
             for monogram2 in monogram:
                 w_file.write(monogram1 + monogram2 + '\n')
