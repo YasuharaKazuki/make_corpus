d44b76205f8127957bab6103b0f55a43235cb60f
yasuhara kazuki <yasuhara.kazuki@g.sp.m.is.nagoya-u.ac.jp>
2019-03-05 20:01:45 +0900

    modify make_bigram_list.py

--- Make_Corpus/make_bigram_list.py
+++ Make_Corpus/make_bigram_list.py
@@ -16,7 +16,7 @@ def main():
     with open(args.unigram_file, 'r') as unigram_file:
         unigram = [line.rstrip() for line in unigram_file]
 
-    with open(args.outdir + 'bigram.txt', 'w') as w_file:
+    with open(args.outdir + 'bigram_list.txt', 'w') as w_file:
         for unigram1 in unigram:
             for unigram2 in unigram:
                 w_file.write(unigram1 + unigram2 + '\n')
