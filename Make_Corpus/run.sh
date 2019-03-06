c4a0e33f2997a0afe294709d8f3b304f2b09b2e2
yasuhara kazuki <yasuhara.kazuki@g.sp.m.is.nagoya-u.ac.jp>
2019-03-06 21:09:13 +0900

    update run.sh

--- Make_Corpus/run.sh
+++ Make_Corpus/run.sh
@@ -2,12 +2,19 @@
 
 # [ USER SETTING ]
 # 0: data preprocess
+# 1: text extraction
 
 stage=1
 
 # [ PREPROCESS SETTING ]
 N_GRAM_DIR=N_gram
+DATA_PREPROCESS=Data_preprocess
 
+# [ EXTRACTION SETTING ]
+n_jobs=4
+max_mora=30
+min_mora=20
+path_file=path_list.txt
 
 # [ STAGE 0 ]
 if echo ${stage} | grep -q 0; then
@@ -24,3 +31,18 @@ if echo ${stage} | grep -q 0; then
 
 fi
 
+# [ STAGE 1 ]
+if echo ${stage} | grep -q 1; then
+    echo "########################################"
+    echo "#            TEXT EXTRACTION           #"
+    echo "########################################"
+
+    [ -e ${path_file} ] && rm ${path_file}
+    find ${DATA_PREPROCESS} | grep txt | head -n1000 > ${path_file}
+
+    python test.py \
+        --n_jobs ${n_jobs} \
+        --path_file ${path_file} \
+        --max_mora ${max_mora} \
+        --min_mora ${min_mora} > test.txt
+fi
